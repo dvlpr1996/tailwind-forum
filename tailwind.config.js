b462663442a9a00a3aeee8a17993baa4e912771d
dvlpr1996 <nimajahanbakhshian@gmail.com>
2023-01-06 17:10:32 +0330

    init

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./*.html'
+	],
+	theme: {
+		screens: {
+			'xs': '360px',
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
+		},
+		extend: {},
+	},
+
+	plugins: [
+		require('tailwindcss-debug-screens'),
+		require('@tailwindcss/line-clamp')
+	],
+}
